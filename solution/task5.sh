b510cd4175c4d95a6ccf0fd72855068d3097e6cf
Egor Savin <savin.es2007@gmail.com>
2025-09-14 00:52:26 +0700

    Update solution

--- solution/task5.sh
+++ solution/task5.sh
@@ -1,4 +1,3 @@
 cat data/students.csv > data/students.txt
-sed -i 's/,/ /' data/students.txt
-sed -i -E 's/(.*) (.*) (.*)/\2 \1 (\3)/' data/students.txt
-sed -i -E 's/(.*) (.*) (.*) (.*)/\3 \2 (\4)/' data/students.txt
+sed -i -E 's/(.*) (.*) (.*),(.*)/\2 \1 (\4)/' data/students.txt
+sed -i -E 's/(.*) (.*),(.*)/\2 \1 (\3)/' data/students.txt
